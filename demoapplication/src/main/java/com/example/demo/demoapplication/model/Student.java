821db0e7d6c9ad3daf0f8de0627bcb61ab649074
sandeep singh <singh18sandy@gmail.com>
2021-03-17 12:35:25 +0530

    Integrated lombok in Spring Boot Demo Project

---
+++ demoapplication/src/main/java/com/example/demo/demoapplication/model/Student.java
@@ -0,0 +1,12 @@
+package com.example.demo.demoapplication.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+	
+	private int studentId;
+	private String name;
+	private String course;
+
+}
