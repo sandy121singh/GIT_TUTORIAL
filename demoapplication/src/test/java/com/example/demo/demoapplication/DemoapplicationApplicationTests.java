b42be981dccfeb992cc8b6a94186ebf2e37d0d72
sandeep singh <singh18sandy@gmail.com>
2021-03-17 11:55:48 +0530

    Spring Boot Demo Project

---
+++ demoapplication/src/test/java/com/example/demo/demoapplication/DemoapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.demoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
